0e87ce1a45b47c0fc97529df85e3c973be791721
polashds <polashtechai@gmail.com>
2024-12-29 17:52:59 +0600

    app modified

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 
 from src.dsproject.logger import logging
-
+from src.dsproject.exception import CustomException
+import sys
 
 if __name__=="__main__":
     logging.info("the execution has started")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
 
